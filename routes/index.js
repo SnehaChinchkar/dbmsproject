b6bfe06e90950b79970218537dedd06daa59aae3
SnehaChinchkar <snehachinchkar3@gmail.com>
2024-04-18 08:13:19 +0530

    profile page function added

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,15 @@ const passport = require('passport');
 const localStrategy= require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
 
+
+
+// router.get('/search_result', async function(req, res) {
+//   const result1 = await userModel.find({ Departure_station_id: req.trains.Departure_station_id ,Arrival_station_id:req.trains.Arrival_station_id });
+  
+//   const dep_station= result1.Departure_station_id;
+//   res.render('search_result', { depstation:result1[0].Departure_station_id, arrstation:result1[0].Arrival_station_id, train:result1[0].Train_id}); // Pass the username to the profile page
+// });
+
 router.get('/', function(req, res, next) {
   res.render('home');
 });
@@ -17,16 +26,21 @@ router.get('/login-page', function(req, res, next) {
 router.get('/register-page',function(req,res){
   res.render('registerpg');
 })
-router.get('/profile',isLoggedIn,function(req,res,next){
-  res.render('profile');
-})
-router.post('/search-train',function(req,res){
-  var trainfound=new trainModel({
-    Departure_station_id:req.body.Departure_station_id,
-    Arrival_station_id:req.body.Arrival_station_id
-    // Departure_day:req.body.Departure_day
-  })
-})
+// router.get('/profile',isLoggedIn,function(req,res,next){
+//   res.render('profile');
+// })
+
+router.get('/profile', isLoggedIn, async function(req, res) {
+  const result1 = await userModel.find({ username: req.user.username });
+  
+  const emailid= result1.email;
+  res.render('profile', { username: req.user.username , email:result1[0].email}); // Pass the username to the profile page
+});
+
+
+
+
+
 router.post('/register',function(req,res){
 var userdata= new userModel({
   username:req.body.username,
